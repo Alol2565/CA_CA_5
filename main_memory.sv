c45f3fce78d93acc6c34dabb9920b9a4565bd092
Ali Saei <alisaei90@gmail.com>
2020-08-05 11:46:51 +0430

    Update main_memory.sv

--- main_memory.sv
+++ main_memory.sv
@@ -6,6 +6,6 @@ module mainmem(input[12:0] address, input clk,rst, output reg [127:0] memory_out
    $readmemb ("main_memory.data", memoryData);
   end
   always@(address) begin
-    memory_out = {memoryData[{address,2'b00}], memoryData[{init_address,2'b01}], memoryData[{init_address,2'b10}], memoryData[{init_address,2'b11}]};
+    memory_out = {memoryData[{address,2'b00}], memoryData[{address,2'b01}], memoryData[{address,2'b10}], memoryData[{address,2'b11}]};
   end
 endmodule
