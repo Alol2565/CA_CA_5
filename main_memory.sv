cc97e9788a403e33c85026e7b92afe4ea6d03b7f
Ali Saei <alisaei90@gmail.com>
2020-08-05 12:10:53 +0430

    Update main_memory.sv

--- main_memory.sv
+++ main_memory.sv
@@ -1,11 +1,11 @@
 `timescale 1ns/1ns
 module mainmem(input[12:0] address, input clk,rst, output reg [127:0] memory_out);
-  reg [31:0] memoryData [32767:0];
+  reg [31:0] memory_data [32767:0];
   initial
   begin
-   $readmemb ("main_memory.data", memoryData);
+   $readmemb ("main_memory.data", memory_data);
   end
   always@(address) begin
-    memory_out = {memoryData[{address,2'b00}], memoryData[{address,2'b01}], memoryData[{address,2'b10}], memoryData[{address,2'b11}]};
+    memory_out = {memory_data[{address,2'ab00}], memory_data[{address,2'b01}], memory_data[{address,2'b10}], memory_data[{address,2'b11}]};
   end
 endmodule
