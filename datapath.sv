8ca5758b23df0a0636ef4109d29984c478f5c924
Ali Saei <alisaei90@gmail.com>
2020-08-06 14:21:18 +0430

    Delete ADD

--- datapath.sv
+++ datapath.sv
@@ -6,15 +6,14 @@ module datapath(input clk, output reg [31:0] output_data);
   wire [9:0] index;
   wire [1:0] word_offset;
   wire [127:0] memory_out;
-  wire [12:0] prime_address;
   wire [31:0] cache_data_out;
   wire hit;
   integer hit_number = 0;
   integer access_number = 0;
   assign output_data = cache_data_out;
   array array (clk, address);
-  AddressDecoder AddressDecoder (address, tag, index, word_offset, prime_address);
-  mainmem mainmem (prime_address, memory_out);
+  AddressDecoder AddressDecoder (address, tag, index, word_offset);
+  mainmem mainmem ({tag,index}, memory_out);
   cache cache (clk, memory_out, tag, index, word_offset, cache_data_out,hit);
   always@(negedge clk) begin
     if(hit) hit_number = hit_number + 1;
